5dfd14cf4715659040293ea6648e5e072e546007
Martin Pihrt <martinpihrt@seznam.cz>
2015-07-01 10:17:26 +0200

    Update __init__.py
    Add max number record in debug file

--- plugins/system_debug/__init__.py
+++ plugins/system_debug/__init__.py
@@ -19,7 +19,8 @@ debug_options = PluginOptions(
         "debug_event": True,
         "info_event": True,
         "warning_event": True,
-        "error_event": True
+        "error_event": True,
+        "log_records": 0,
     }
 )
 
@@ -38,6 +39,20 @@ def get_overview():
     """Returns the info data as a list of lines."""
     result = []     
     try:
+        if debug_options['log_records'] > 0:     # delete lines in debug log if records is bigger
+          file = open(log.EVENT_FILE,"r")
+          num_lines = sum(1 for line in file)
+          num_records = debug_options['log_records']
+          data = file.readlines()
+          file.close()
+          print num_lines
+          if num_lines > num_records:
+             del data[:num_lines-(num_records-1)] 
+             fout = open(log.EVENT_FILE, "w")
+             fout.writelines(data)
+             fout.close()
+             raise web.seeother(plugin_url(status_page), True)
+                   
         for line in reversed(list(open(log.EVENT_FILE))):
             if debug_options['debug_event']:
               if 'DEBUG' in line:
@@ -57,7 +72,7 @@ def get_overview():
 
            
     except Exception:
-        result.append('Error: Log file missing.')
+        result.append('Error: Log file missing. Enable it in system options.')
 
     return result
 
@@ -81,7 +96,7 @@ class status_page(ProtectedPage):
 
 
 class settings_page(ProtectedPage):
-    """Save an html page for entering debug filtering."""
+    """Save an html page for entering debug filtering or submit change."""
 
     def POST(self):
         debug_options.web_update(web.input())
