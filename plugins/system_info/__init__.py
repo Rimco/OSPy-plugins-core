a4f8536df796a2862883b75c0d89fc5d11a70f2a
Martin Pihrt <martinpihrt@seznam.cz>
2015-09-24 13:49:09 +0200

    Update __init__.py
    fix error if debug log in options is disabled.
    oprava chyby - nezobrazoval se výpis, když v options byl zakázán debug log výpis

--- plugins/system_info/__init__.py
+++ plugins/system_info/__init__.py
@@ -27,33 +27,35 @@ stop = start
 def get_overview():
     """Returns the info data as a list of lines."""
     result = []
-    log.clear(NAME)
-    meminfo = helpers.get_meminfo()
-    netdevs = helpers.get_netdevs()
-
-    result.append('System release: ' + platform.release())
-    result.append('System name:    ' + platform.system())
-    result.append('Node:           ' + platform.node())
-    result.append('Machine:        ' + platform.machine())
-    result.append('Distribution:   ' + (platform.linux_distribution()[0]) + ' ' + (platform.linux_distribution()[1]))
-    result.append('Total memory:   ' + meminfo['MemTotal'])
-    result.append('Free memory:    ' + meminfo['MemFree'])
-    if netdevs:
-        for dev, info in netdevs.iteritems():
-            result.append('%-16s %s MiB %s MiB' % (dev + ':', info['rx'], info['tx']))
-    else:
-        result.append('Network:        Unknown')
-    result.append('Uptime:         ' + helpers.uptime())
-    result.append('CPU temp:       ' + helpers.get_cpu_temp(options.temp_unit) + ' ' + options.temp_unit)
-    result.append('MAC adress: ' + helpers.get_mac())
     try:
-        result.append('I2C HEX Adress:')
-        rev = str(0 if helpers.get_rpi_revision() == 1 else 1)
-        cmd = 'sudo i2cdetect -y ' + rev
-        result.append(process(cmd))
-    except Exception:
-        log.error(NAME, 'System info plug-in:\n' + traceback.format_exc())
-    log.info(NAME, result)
+        meminfo = helpers.get_meminfo()
+        netdevs = helpers.get_netdevs()
+
+        result.append('System release: ' + platform.release())
+        result.append('System name:    ' + platform.system())
+        result.append('Node:           ' + platform.node())
+        result.append('Machine:        ' + platform.machine())
+        result.append('Distribution:   ' + (platform.linux_distribution()[0]) + ' ' + (platform.linux_distribution()[1]))
+        result.append('Total memory:   ' + meminfo['MemTotal'])
+        result.append('Free memory:    ' + meminfo['MemFree'])
+        if netdevs:
+            for dev, info in netdevs.iteritems():
+                result.append('%-16s %s MiB %s MiB' % (dev + ':', info['rx'], info['tx']))
+        else:
+            result.append('Network:        Unknown')
+        result.append('Uptime:         ' + helpers.uptime())
+        result.append('CPU temp:       ' + helpers.get_cpu_temp(options.temp_unit) + ' ' + options.temp_unit)
+        result.append('MAC adress: ' + helpers.get_mac())
+        try:
+            result.append('I2C HEX Adress:')
+            rev = str(0 if helpers.get_rpi_revision() == 1 else 1)
+            cmd = 'sudo i2cdetect -y ' + rev
+            result.append(process(cmd))
+        except Exception:
+          result.append('System info plug-in:\n' + traceback.format_exc())
+
+    except:
+        result.append('System info plug-in:\n' + traceback.format_exc())
     return result
 
 
